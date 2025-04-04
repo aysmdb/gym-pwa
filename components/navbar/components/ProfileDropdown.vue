9c085ecc3879cf1eaae0f31e93ad1133caa443d3
aysmdb <aysmdb@gmail.com>
2025-04-04 08:33:47 +0700

    profile dropdown comp

--- components/navbar/components/ProfileDropdown.vue
+++ components/navbar/components/ProfileDropdown.vue
@@ -11,10 +11,90 @@
                     </span>
                 </VaButton>
             </template>
+            <VaDropdownContent 
+            class="profile-dropdown__content md:w-60 px-0 py-4 w-full"
+                :style="{ '--hover-color': hoverColor }">
+                <VaList v-for="group in options" :key="group.name">
+                    <header 
+                    v-if="group.name"
+                        class="uppercase text-[var(--va-secondary)] opacity-80 font-bold text-xs px-4">
+                        {{ group.name }}
+                    </header>
+                    <VaListItem
+                    v-for="item in group.list"
+                    :key="item.name"
+                    class="menu-item px-4 text-base cursor-pointer h-8"
+                    v-bind="resolveLink(item)">
+                    <VaIcon :name="item.icon" class="pr-1" color="secondary">
+                        {{  item.name }}
+                    </VaIcon>
+                    </VaListItem>
+                    <VaSeparator v-if="group.separator" class="mx-3 my-2" />
+                </VaList>
+            </VaDropdownContent>
         </VaDropdown>
     </div>
 </template>
 
 <script setup lang="ts">
+import { useColors } from 'vuestic-ui';
+
+const { colors, setHSLAColor } = useColors();
+const hoverColor = computed(() => setHSLAColor(colors.focus, { a: 0.1 }))
 const isShown = useState('isShown', () => false);
+
+type ProfileListItem = {
+    name: string;
+    to?: string;
+    href?: string;
+    icon: string;
+}
+
+type ProfileOptions = {
+    name: string;
+    separator: boolean;
+    list: ProfileListItem[];
+}
+
+withDefaults(
+    defineProps<{
+        options?: ProfileOptions[];
+    }>(),
+    {
+        options: () => [
+            {
+                name: 'account',
+                separator: true,
+                list: [
+                    {
+                        name: 'profile',
+                        to: 'preferences',
+                        icon: 'mso-account_circle',
+                    },
+                    {
+                        name: 'settings',
+                        to: 'settings',
+                        icon: 'mso-settings',
+                    },
+                ],
+            },
+            {
+                name: '',
+                separator: false,
+                list: [
+
+                    {
+                        name: 'logout',
+                        to: 'login',
+                        icon: 'mso-logout',
+                    },
+                ],
+            },
+        ]
+    }
+);
+
+const resolveLink = (item: ProfileListItem) => {
+    return item.to ? { to: { name: item.to } } : item.href ? { href: item.href, target: '_blank' } : {};
+};
 </script>
