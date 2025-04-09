1e2d1bddd3358fc829241646c88d37abe6c1056c
iyan <aysmdb@gmail.com>
2025-04-09 20:09:05 +0700

    sidebar

--- components/sidebar/AppSidebar.vue
+++ components/sidebar/AppSidebar.vue
@@ -1,45 +1,33 @@
 <template>
-  <VaSidebar v-model="writableVisible" :width="sidebarWidth" :color="color" minimized-width="0">
+  <VaSidebar :width="sidebarWidth" :color="color" minimized-width="0">
     <VaAccordion v-model="value" multiple>
-      <VaCollapse v-for="(route, index) in navigationRoutes.routes" :key="index">
+      <VaCollapse v-for="(r, index) in navigationRoutes.routes" :key="index">
         <template #header="{ value: isCollapsed }">
-          <VaSidebarItem
-            :to="route.children ? undefined : { name: route.name }"
-            :active="routeHasActiveChild(route)"
-            :active-color="activeColor"
-            :text-color="textColor(route)"
-            :aria-label="`${route.children ? 'Open category ' : 'Visit'} ${t(route.displayName)}`"
-            role="button"
-            hover-opacity="0.10"
-          >
+          <VaSidebarItem 
+            :to="r.children ? undefined : { name: route.name }" :active="routeHasActiveChild(r)"
+            :active-color="activeColor" :text-color="textColor(r)"
+            :aria-label="`${r.children ? 'Open category ' : 'Visit'} ${r.displayName}`" role="button"
+            hover-opacity="0.10">
             <VaSidebarItemContent class="py-3 pr-2 pl-4">
-              <VaIcon
-                v-if="route.meta.icon"
-                aria-hidden="true"
-                :name="route.meta.icon"
-                size="20px"
-                :color="iconColor(route)"
-              />
+              <VaIcon 
+                v-if="r.meta.icon" aria-hidden="true" :name="r.meta.icon" size="20px"
+                :color="iconColor(r)" />
               <VaSidebarItemTitle class="flex justify-between items-center leading-5 font-semibold">
-                {{ t(route.displayName) }}
-                <VaIcon v-if="route.children" :name="arrowDirection(isCollapsed)" size="20px" />
+                {{ r.displayName }}
+                <VaIcon v-if="r.children" :name="arrowDirection(isCollapsed)" size="20px" />
               </VaSidebarItemTitle>
             </VaSidebarItemContent>
           </VaSidebarItem>
         </template>
         <template #body>
-          <div v-for="(childRoute, index2) in route.children" :key="index2">
-            <VaSidebarItem
-              :to="{ name: childRoute.name }"
-              :active="isActiveChildRoute(childRoute)"
-              :active-color="activeColor"
-              :text-color="textColor(childRoute)"
-              :aria-label="`Visit ${t(route.displayName)}`"
-              hover-opacity="0.10"
-            >
+          <div v-for="(childRoute, index2) in r.children" :key="index2">
+            <VaSidebarItem 
+              :to="{ name: childRoute.name }" :active="isActiveChildRoute(childRoute)"
+              :active-color="activeColor" :text-color="textColor(childRoute)"
+              :aria-label="`Visit ${r.displayName}`" hover-opacity="0.10">
               <VaSidebarItemContent class="py-3 pr-2 pl-11">
                 <VaSidebarItemTitle class="leading-5 font-semibold">
-                  {{ t(childRoute.displayName) }}
+                  {{ childRoute.displayName }}
                 </VaSidebarItemTitle>
               </VaSidebarItemContent>
             </VaSidebarItem>
@@ -53,7 +41,7 @@
 import { useColors } from 'vuestic-ui'
 import navigationRoutes, { type INavigationRoute } from './routes'
 
-withDefaults(defineProps<{
+const props = withDefaults(defineProps<{
   visible?: boolean
   mobile?: boolean
 }>(), {
@@ -64,100 +52,28 @@ withDefaults(defineProps<{
 const { getColor, colorToRgba } = useColors()
 const route = useRoute()
 
-    const writableVisible = computed({
-      get: () => props.visible,
-      set: (v: boolean) => emit('update:visible', v),
-    })
+const isActiveChildRoute = (child: INavigationRoute) => route.name === child.name
 
-    const isActiveChildRoute = (child: INavigationRoute) => route.name === child.name
+const routeHasActiveChild = (section: INavigationRoute) => {
+  if (!section.children) {
+    return route.path.endsWith(`${section.name}`)
+  }
 
-    const routeHasActiveChild = (section: INavigationRoute) => {
-      if (!section.children) {
-        return route.path.endsWith(`${section.name}`)
-      }
+  return section.children.some(({ name }) => route.path.endsWith(`${name}`))
+}
 
-      return section.children.some(({ name }) => route.path.endsWith(`${name}`))
-    }
+const value = useState('value', (): boolean[] => [])
 
-    const setActiveExpand = () =>
-      (value.value = navigationRoutes.routes.map((route: INavigationRoute) => routeHasActiveChild(route)))
+const setActiveExpand = () =>
+  (value.value = navigationRoutes.routes.map((route: INavigationRoute) => routeHasActiveChild(route)))
 
-    const sidebarWidth = computed(() => (props.mobile ? '100vw' : '280px'))
-    const color = computed(() => getColor('background-secondary'))
-    const activeColor = computed(() => colorToRgba(getColor('focus'), 0.1))
+const sidebarWidth = computed(() => (props.mobile ? '100vw' : '280px'))
+const color = computed(() => getColor('background-secondary'))
+const activeColor = computed(() => colorToRgba(getColor('focus'), 0.1))
 
-    const iconColor = (route: INavigationRoute) => (routeHasActiveChild(route) ? 'primary' : 'secondary')
-    const textColor = (route: INavigationRoute) => (routeHasActiveChild(route) ? 'primary' : 'textPrimary')
-    const arrowDirection = (state: boolean) => (state ? 'va-arrow-up' : 'va-arrow-down')
+const iconColor = (route: INavigationRoute) => (routeHasActiveChild(route) ? 'primary' : 'secondary')
+const textColor = (route: INavigationRoute) => (routeHasActiveChild(route) ? 'primary' : 'textPrimary')
+const arrowDirection = (state: boolean) => (state ? 'va-arrow-up' : 'va-arrow-down')
 
-    watch(() => route.fullPath, setActiveExpand, { immediate: true })
-
-
-// import { defineComponent, watch, ref, computed } from 'vue'
-// import { useRoute } from 'vue-router'
-
-// import { useI18n } from 'vue-i18n'
-// import { useColors } from 'vuestic-ui'
-
-// import navigationRoutes, { type INavigationRoute } from './NavigationRoutes'
-
-// export default defineComponent({
-//   name: 'Sidebar',
-//   props: {
-//     visible: { type: Boolean, default: true },
-//     mobile: { type: Boolean, default: false },
-//   },
-//   emits: ['update:visible'],
-
-//   setup: (props, { emit }) => {
-//     const { getColor, colorToRgba } = useColors()
-//     const route = useRoute()
-//     const { t } = useI18n()
-
-//     const value = ref<boolean[]>([])
-
-//     const writableVisible = computed({
-//       get: () => props.visible,
-//       set: (v: boolean) => emit('update:visible', v),
-//     })
-
-//     const isActiveChildRoute = (child: INavigationRoute) => route.name === child.name
-
-//     const routeHasActiveChild = (section: INavigationRoute) => {
-//       if (!section.children) {
-//         return route.path.endsWith(`${section.name}`)
-//       }
-
-//       return section.children.some(({ name }) => route.path.endsWith(`${name}`))
-//     }
-
-//     const setActiveExpand = () =>
-//       (value.value = navigationRoutes.routes.map((route: INavigationRoute) => routeHasActiveChild(route)))
-
-//     const sidebarWidth = computed(() => (props.mobile ? '100vw' : '280px'))
-//     const color = computed(() => getColor('background-secondary'))
-//     const activeColor = computed(() => colorToRgba(getColor('focus'), 0.1))
-
-//     const iconColor = (route: INavigationRoute) => (routeHasActiveChild(route) ? 'primary' : 'secondary')
-//     const textColor = (route: INavigationRoute) => (routeHasActiveChild(route) ? 'primary' : 'textPrimary')
-//     const arrowDirection = (state: boolean) => (state ? 'va-arrow-up' : 'va-arrow-down')
-
-//     watch(() => route.fullPath, setActiveExpand, { immediate: true })
-
-//     return {
-//       writableVisible,
-//       sidebarWidth,
-//       value,
-//       color,
-//       activeColor,
-//       navigationRoutes,
-//       routeHasActiveChild,
-//       isActiveChildRoute,
-//       t,
-//       iconColor,
-//       textColor,
-//       arrowDirection,
-//     }
-//   },
-// })
+watch(() => route.fullPath, setActiveExpand, { immediate: true })
 </script>
